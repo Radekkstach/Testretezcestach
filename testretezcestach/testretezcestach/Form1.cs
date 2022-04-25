5a596fa7f7b69c7d11772b50d9dafc38d4a2b4d4
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2022-04-25 09:34:06 +0200

    Add files via upload

---
+++ testretezcestach/testretezcestach/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testretezcestach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string text = textBox1.Text;
+            string abecedniZnaky = "a b c d e f g h i j k l m n o p q r s t u v w x y z ";
+            text = text.Trim();
+            text = text.ToLower();
+            //int pocet = 0;
+            //int predchozi = 0;
+            //char predchozip = "";
+            
+            while(text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+            
+            string[] slova = text.Split(' ');
+            foreach(string slovicka in slova)
+            {
+                foreach(char pismeno in slovicka)
+                {
+                    if (abecedniZnaky.Contains(pismeno))
+                    {
+
+                        //if (pismeno == predchozip)
+                        //{
+                        //    pocet++;
+                        //}
+                        //else
+                        //{
+                        //    predchozi = pocet;
+                        //    pocet = 1;
+                        //}
+                        //predchozip = pismeno;
+                        
+                        abecedniZnaky = abecedniZnaky.Replace(pismeno, ' ');
+                    }
+                    
+                 
+
+                }
+
+                
+                
+            }
+            label2.Text = text;
+            label1.Text ="Pismena co ti zbyvaji: " + abecedniZnaky;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string abecedniZnaky = "a b c d e f g h i j k l m n o p q r s t u v w x y z ";
+            string dalsiznak = textBox2.Text;
+            dalsiznak = dalsiznak.Trim();
+            if (dalsiznak.Length == 1)
+            {
+                char znak = char.Parse(dalsiznak);
+                if (dalsiznak.Length == 1 && abecedniZnaky.Contains(dalsiznak))
+                {
+                    label2.Text = label2.Text.Replace('_', znak);
+                }
+                else
+                {
+                    MessageBox.Show("Zadej pouze jedno pismeno");
+                }
+                label1.Text = label1.Text.Replace(znak, ' ');
+            }
+            else
+            {
+                MessageBox.Show("Zadej pouze jedno pismeno");
+            }
+
+        }
+    }
+}
---
+++ testretezcestach/testretezcestach/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("testretezcestach")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testretezcestach")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("d7bad81c-6b51-40ef-aa17-2e9f34f578c2")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
